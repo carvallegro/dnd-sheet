a529d6f3a685f3ed3a394aae21ef76a5bc12715b
Carvallegro <carvallegro@users.noreply.github.com>
2018-12-23 16:52:40 +0100

    first commit

---
+++ src/components/common/layout/index.js
@@ -0,0 +1,10 @@
+import styled from'styled-components'
+
+export const Page = styled.div`
+  height: 210mm;
+  width: 148mm;
+  background-color: white;
+  padding: 20px;
+  display: grid;
+  grid-gap: 10pt;
+`
---
+++ src/components/common/typography/index.js
@@ -0,0 +1,8 @@
+import styled from'styled-components'
+
+import {typography} from '../../styles'
+
+export const PageHeading = styled.h1`
+  ${typography.Heading}
+  margin: 0;
+`
