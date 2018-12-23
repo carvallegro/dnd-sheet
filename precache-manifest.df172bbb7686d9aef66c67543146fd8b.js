e537267d6b6e3df9a2693a613a5b2f064928271d
Carvallegro <carvallegro@users.noreply.github.com>
2018-12-23 16:58:55 +0100

    Updates

---
+++ precache-manifest.df172bbb7686d9aef66c67543146fd8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "75d04330b0f069b2b352413aafc488ae",
+    "url": "/dnd-sheet/static/media/sub_shield.75d04330.svg"
+  },
+  {
+    "revision": "4361646896dc7559984f13cf2f087185",
+    "url": "/dnd-sheet/static/media/shield.43616468.svg"
+  },
+  {
+    "revision": "caa909719039262559d9",
+    "url": "/dnd-sheet/static/js/runtime~main.caa90971.js"
+  },
+  {
+    "revision": "ae145b87aef205df1cc1",
+    "url": "/dnd-sheet/static/js/main.ae145b87.chunk.js"
+  },
+  {
+    "revision": "d095a368b8c0ae82f736",
+    "url": "/dnd-sheet/static/js/1.d095a368.chunk.js"
+  },
+  {
+    "revision": "22350658794e0427a3ddd283e0fa080b",
+    "url": "/dnd-sheet/index.html"
+  }
+];
--- src/components/common/typography/index.js
+++
@@ -1,8 +0,0 @@
-import styled from'styled-components'
-
-import {typography} from '../../styles'
-
-export const PageHeading = styled.h1`
-  ${typography.Heading}
-  margin: 0;
-`
