657a7f44545b3dba44de33d5c7480e5499b18636
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-26 15:45:43 +0000

    Deploy carvallegro/dnd-sheet to github.com/carvallegro/dnd-sheet.git:gh-pages

---
+++ precache-manifest.d411fe300603cb0550a1655d4e92ac17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "75d04330b0f069b2b352413aafc488ae",
+    "url": "/dnd-sheet/static/media/sub_shield.75d04330.svg"
+  },
+  {
+    "revision": "4361646896dc7559984f13cf2f087185",
+    "url": "/dnd-sheet/static/media/shield.43616468.svg"
+  },
+  {
+    "revision": "caa909719039262559d9",
+    "url": "/dnd-sheet/static/js/runtime~main.caa90971.js"
+  },
+  {
+    "revision": "63c869aed752293e8ce3",
+    "url": "/dnd-sheet/static/js/main.63c869ae.chunk.js"
+  },
+  {
+    "revision": "9a84f3e699ad9bf6f864",
+    "url": "/dnd-sheet/static/js/1.9a84f3e6.chunk.js"
+  },
+  {
+    "revision": "6f294da9ada6985f0fa8cafd39480fd7",
+    "url": "/dnd-sheet/index.html"
+  }
+];
