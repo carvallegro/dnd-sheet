c61c6d3e683f2ac1f07daba3d1a8efd292816c46
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-31 17:17:35 +0000

    Deploy carvallegro/dnd-sheet to github.com/carvallegro/dnd-sheet.git:gh-pages

--- precache-manifest.431960e64b974b13011597dfa8aae015.js
+++ precache-manifest.431960e64b974b13011597dfa8aae015.js
@@ -20,15 +20,15 @@ self.__precacheManifest = [
     "url": "/dnd-sheet/static/js/runtime~main.caa90971.js"
   },
   {
-    "revision": "856a5782fb2fcdab15ef",
-    "url": "/dnd-sheet/static/js/main.856a5782.chunk.js"
+    "revision": "d68daac0b3a03578bdfc",
+    "url": "/dnd-sheet/static/js/main.d68daac0.chunk.js"
   },
   {
-    "revision": "5762acdfb798f3e8b267",
-    "url": "/dnd-sheet/static/js/1.5762acdf.chunk.js"
+    "revision": "9b3502b9b26a68c86f7e",
+    "url": "/dnd-sheet/static/js/1.9b3502b9.chunk.js"
   },
   {
-    "revision": "8167e0417053ea584327ad24376e7fc1",
+    "revision": "2daaa636f47f4ed73e2ab5f926c050b0",
     "url": "/dnd-sheet/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dnd-sheet/precache-manifest.53f92678d83603fee6c214649d9b48fe.js"
+  "/dnd-sheet/precache-manifest.431960e64b974b13011597dfa8aae015.js"
 );
 
 workbox.clientsClaim();
